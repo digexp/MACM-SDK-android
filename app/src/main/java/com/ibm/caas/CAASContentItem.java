38af0510a6494fb90f0141c4c0600ddf579289cd
Laurent Cohen <laurentc@fr.ibm.com>
2015-11-05 08:58:11 +0100

    javadoc fixes - missed CAASContentItem in commit

--- app/src/main/java/com/ibm/caas/CAASContentItem.java
+++ app/src/main/java/com/ibm/caas/CAASContentItem.java
@@ -28,7 +28,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Encapsulates the response received from the (WCM) server.
+ * Instances of this class represent content items retrieved from the server.
  */
 public class CAASContentItem implements Serializable {
   enum Property {
@@ -99,114 +99,226 @@ public class CAASContentItem implements Serializable {
     return getProperty(Property.OID);
   }
 
+  /**
+   * Return this content item's name as a string,
+   * or <code>null</code> if the name is not defined.
+   */
   public String getName() {
     return getProperty(Property.NAME);
   }
 
+  /**
+   * Return this content item's "contenttype" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getContentType() {
     return getProperty(Property.CONTENT_TYPE);
   }
 
+  /**
+   * Return this content item's "authors" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getAuthors() {
     return getProperty(Property.AUTHORS);
   }
 
+  /**
+   * Return this content item's "title" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getTitle() {
     return getProperty(Property.TITLE);
   }
 
+  /**
+   * Return this content item's "authtemplateid" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getAuthTemplateId() {
     return getProperty(Property.AUTH_TEMPLATE_ID);
   }
 
+  /**
+   * Return this content item's "authtemplatename" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getAuthTemplateName() {
     return getProperty(Property.AUTH_TEMPLATE_NAME);
   }
 
+  /**
+   * Return this content item's "authtemplatetitle" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getAuthTemplateTitle() {
     return getProperty(Property.AUTH_TEMPLATE_TITLE);
   }
 
+  /**
+   * Return this content item's "categories" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getCategories() {
     return getProperty(Property.CATEGORIES);
   }
 
+  /**
+   * Return this content item's "creationdate" property as a {@link Date},
+   * or <code>null</code> if the property is not defined.
+   */
   public Date getCreationDate() {
     return getProperty(Property.CREATION_DATE);
   }
 
+  /**
+   * Return this content item's "creator" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getCreator() {
     return getProperty(Property.CREATOR);
   }
 
+  /**
+   * Return this content item's "currentstage" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getCurrentStage() {
     return getProperty(Property.CURRENT_STAGE);
   }
 
+  /**
+   * Return this content item's "description" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getDescription() {
     return getProperty(Property.DESCRIPTION);
   }
 
+  /**
+   * Return this content item's "expirydate" property as a {@link Date},
+   * or <code>null</code> if the property is not defined.
+   */
   public Date getExpiryDate() {
     return getProperty(Property.EXPIRY_DATE);
   }
 
+  /**
+   * Return this content item's "keywords" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getKeywords() {
     return getProperty(Property.KEYWORDS);
   }
 
+  /**
+   * Return this content item's "lastmodifieddate" property as a {@link Date},
+   * or <code>null</code> if the property is not defined.
+   */
   public Date getLastModifiedDate() {
     return getProperty(Property.LAST_MODIFIED_DATE);
   }
 
+  /**
+   * Return this content item's "lastmodifier" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getLastModifier() {
     return getProperty(Property.LAST_MODIFIER);
   }
 
+  /**
+   * Return this content item's "libraryid" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getLibraryId() {
     return getProperty(Property.LIBRARY_ID);
   }
 
+  /**
+   * Return this content item's "libraryname" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getLibraryName() {
     return getProperty(Property.LIBRARY_NAME);
   }
 
+  /**
+   * Return this content item's "librarytitle" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getLibraryTitle() {
     return getProperty(Property.LIBRARY_TITLE);
   }
 
+  /**
+   * Return this content item's "parentid" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getParentId() {
     return getProperty(Property.PARENT_ID);
   }
 
+  /**
+   * Return this content item's "parentname" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getParentName() {
     return getProperty(Property.PARENT_NAME);
   }
 
+  /**
+   * Return this content item's "parenttitle" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getParentTitle() {
     return getProperty(Property.PARENT_TITLE);
   }
 
+  /**
+   * Return this content item's "projectid" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getProjectId() {
     return getProperty(Property.PROJECT_ID);
   }
 
+  /**
+   * Return this content item's "projectname" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getProjectName() {
     return getProperty(Property.PROJECT_NAME);
   }
 
+  /**
+   * Return this content item's "projecttitle" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getProjectTitle() {
     return getProperty(Property.PROJECT_TITLE);
   }
 
+  /**
+   * Return this content item's "publishdate" property as a {@link Date},
+   * or <code>null</code> if the property is not defined.
+   */
   public Date getPublishDate() {
     return getProperty(Property.PUBLISH_DATE);
   }
 
+  /**
+   * Return this content item's "status" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getStatus() {
     return getProperty(Property.STATUS);
   }
 
+  /**
+   * Return this content item's "statusid" property as a string,
+   * or <code>null</code> if the property is not defined.
+   */
   public String getStatusId() {
     return getProperty(Property.STATUS_ID);
   }
@@ -229,12 +341,24 @@ public class CAASContentItem implements Serializable {
     return elements.has(name);
   }
 
+  /**
+   * Get the value of this content item's element
+   * @param name the name of the element to retrieve.
+   * @param <T> the type of the element's value.
+   * @return the element's value or <code>null</code> if the element is not defined.
+   */
   public <T> T getElement(String name) {
     return elements.get(name);
   }
 
-  public <T> T getProperty(String property) {
-    return properties.get(property);
+  /**
+   * Get the value of this content item's property.
+   * @param name the name of the property to retrieve.
+   * @param <T> the type of the property's value.
+   * @return the property's value or <code>null</code> if the property is not defined.
+   */
+  public <T> T getProperty(String name) {
+    return properties.get(name);
   }
 
   <T> T getProperty(Property property) {
